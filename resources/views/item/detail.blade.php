1348b288f505b530db47aea9708e18936d895244
jkihara <jkihara214@gmail.com>
2023-01-25 17:11:57 +0900

    [Fix item page][商品一覧] 商品一覧viewを他ページと統一<BLANK LINE> <body>

--- resources/views/item/detail.blade.php
+++ resources/views/item/detail.blade.php
@@ -1,29 +1,41 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<title>商品一覧</title>
-</head>
-<body>
-<h1>商品詳細</h1>
-<table border='2'>
-<tr>
-<th>商品名</th>
-<th>商品説明</th>
-<th>値段</th>
-<th>在庫</th>
-</tr>
-<tr>
-<td>{{ $item['name'] }}</td>
-<td>{{ $item['explanation'] }}</td>
-<td>{{ $item['price'] }}</td>
-@if ($item['stock'] > 0)
-<td>在庫あり</td>
-@else
-<td>在庫なし</td>
-@endif
-</tr>
-</table>
-<p><a href="{{ route('item.index') }}">戻る</a></p>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('商品一覧') }}</div>
+
+                <div class="card-body">
+                <table style="border-color:#858383 ; border-style:groove; border-width:5px ;">
+                    <thead>
+                        <tr>
+                            <th>商品名</th>
+                            <th>商品説明</th>
+                            <th>値段</th>
+                            <th>在庫</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{ $item['name'] }}</td> 
+                            <td>{{ $item['explanation'] }}</td>
+                            <td>{{ "￥" . $item['price'] }}</td>
+                            @if ($item['stock'] > 0)
+                                <td>在庫あり</td>
+                            @else
+                                <td>在庫なし</td>
+                            @endif
+                        </tr>
+                    </tbody>
+                </table>
+                <a class="btn btn-link" href="{{ route('item.index') }}">
+                    {{ __('戻る') }}
+                </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -1,28 +1,38 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<title>商品一覧</title>
-</head>
-<body>
-<h1>商品一覧</h1>
-<table border='2'>
-<tr>
-<th>商品名</th>
-<th>値段</th>
-<th>在庫</th>
-</tr>
-@foreach ($items as $item)
-<tr>
-<td><a href="{{ route('item.detail', ['id' => $item['id']]) }}">{{ $item['name'] }}</a></td>
-<td>{{ $item['price'] }}</td>
-@if ($item['stock'] > 0)
-<td>在庫あり</td>
-@else
-<td>在庫なし</td>
-@endif
-</tr>
-@endforeach
-</table>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('商品一覧') }}</div>
+
+                <div class="card-body">
+                <table style="border-color:#858383 ; border-style:groove; border-width:5px ;">
+                    <thead>
+                        <tr>
+                            <th>商品名</th>
+                            <th>値段</th>
+                            <th>在庫</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($items as $item)
+                            <tr>
+                                <td><a href="{{ route('item.detail', ['id' => $item['id']]) }}">{{ $item['name'] }}</a></td>
+                                <td>{{ "￥" . $item['price'] }}</td>
+                                @if ($item['stock'] > 0)
+                                    <td>在庫あり</td>
+                                @else
+                                    <td>在庫なし</td>
+                                @endif
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
